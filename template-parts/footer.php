0bfbb55deb6cc826ccf6e57b88023ea1e216fdc1
cneutgens <december217777@hotmail.com>
2017-01-27 14:42:42 -0600

    CN Added footer

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -1,6 +1,59 @@
 
 <footer>
+	<div class="container">
+		<div class="col-md-3">
+			<a class="img-responsive logo" href="#"><img src="img/logo_sm.png" alt="Colour Couture Logo"></a>
+			<small class="text-uppercase small-text"><p>&copy; 2017 Colour Couture Inc.<br>All Rights Reserved.</p>
+			<p>DESIGN + DEV: AB, AC, CN, KSR</p></small>
+		</div> <!-- end col-md-3 -->
 
+		<div class="col-md-6">
+
+			<div class="col-sm-6">
+				<ul class="footer-nav text-uppercase">
+					<li><a href="#">Shop</a></li>
+					<li><a href="#">Collections</a></li>
+					<li><a href="#">Try It On</a></li>
+					<li><a href="#">Where To Buy</a></li>
+					<li><a href="#">Maniblog</a></li>
+				</ul>
+			</div> <!-- end col-sm-6 -->
+
+			<div class="col-sm-6">
+				<ul class="footer-nav">
+					<li><a href="#">about</a></li>
+					<li><a href="#">contact</a></li>
+					<li><a href="#">terms of service</a></li>
+					<li><a href="#">privacy policy</a></li>
+				</ul>
+			</div> <!-- end col-sm-6 -->
+
+		</div> <!-- end col-md-6 -->
+
+		<div class="col-xs-12 col-md-3">
+			<ul class="footer-right text-uppercase">
+				<li><a href="#"></a>Search<br>
+					<form class="form-inline footer-search">
+					  <div class="form-group">
+					    <label class="sr-only" for="searchInputField">Search</label>
+					    <div class="input-group">
+					      <input type="text" class="form-control" id="searchInputField">
+					      <div class="input-group-addon"><button type="submit" class="btn"><i class="fa fa-search fa-2x"></i></button></div>
+					    </div>
+					  </div>
+					</form>
+				</li>
+				<li><a href="#"></a>Follow Us</li>
+					<ul class="social-nav">
+						<li><a href="#"><i class="fa fa-facebook fa-2x"></i></a></li>
+						<li><a href="#"><i class="fa fa-instagram fa-2x"></i></a></li>
+						<li><a href="#"><i class="fa fa-pinterest fa-2x"></i></a></li>
+					</ul>
+			</ul>
+			
+			
+		</div> <!-- end col-md-3 -->
+	</div> <!-- end container -->
 
 </footer>
 <script src="js/jquery-3.1.1.min.js"></script>
